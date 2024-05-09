4f9eabcceb38fc85a8684a4cf017e919866b5bb2
Anas <anassaif.507@gmail.com>
2024-05-09 16:29:25 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const app = new Hono<({
+  Bindings:{
+    DATABASE_URL: string
+  }
+})>
+
+
+app.get('/', (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  
+  return c.json({data: "Hello"})
+})
+
+export default app
